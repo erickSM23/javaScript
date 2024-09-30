85040695766e157a030a582b5d76a94f498fcd08
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-09-30 12:46:58 -0300

    adicionie javaScript

---
+++ aulas/aula011/ex001.js
@@ -0,0 +1,6 @@
+var vel = 200
+console.log(`a velocidade do seu carro é de ${vel}KM/H`)
+if (vel > 100) { //condição simples
+    console.log('você ultrapassou o limite de velocidade (MULTADO!)')
+}
+console.log('use sempre sinto de segurança...')
