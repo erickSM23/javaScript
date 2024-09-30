1706694e1873f67aad1a77731122c65313dd026c
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-09-30 16:03:19 -0300

    exercicio novo criado

--- aulas/aula011/ex002.js
+++ aulas/aula011/ex002.js
@@ -1,4 +1,7 @@
-var país = 'EUA' 
-if  (país == 'brasil') {
-
+var país = 'frança' 
+console.log(`vivendo em ${país}`)
+if (país == 'brasil') {
+    console.log('você é (brasileiro)')
+} else {
+    console.log('você é (estrangeiro)')
 }
