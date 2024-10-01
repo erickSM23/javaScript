98a355da44e7e8412383796f6e474513c87e4642
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-10-01 17:21:21 -0300

    exercicio de presidente finalizado

---
+++ aulas/aula012/ex005.js
@@ -0,0 +1,19 @@
+var votação = 1
+console.log('CANDIDATOS E SEUS NUMEROS PARA VOTAÇÃO LOGO ABAIXO')
+console.log(' BOLSONARO (22)')
+console.log(' LULA (13)')
+console.log(' PABLO MARÇAL (28)')
+switch(votação) {
+    case 22:
+        console.log('votou em (BOLSONARO)')
+        break
+    case 13:
+        console.log('votou em (LULA)')
+        break
+    case 28:
+        console.log('votou em (PABLO MARÇAL)')
+        break
+    default:
+        console.log('DIGITE UM NUMERO VALIDO!')
+        break
+}
