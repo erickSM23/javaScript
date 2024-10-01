d87507003010cb017a7531dc0786e167df2a26c7
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-10-01 16:29:32 -0300

    exercicio de madru, manha e noite finalizado com êxito

--- aulas/aula012/ex003.js
+++ aulas/aula012/ex003.js
@@ -1,8 +1,12 @@
-var horas 
+var horas = 22
 if (horas <= 6) {
     console.log(`agora são ${horas} da madrugada`)
 } else if (horas <= 11) {
     console.log(`agora são ${horas} da manha`)
 } else if (horas <= 18) {
     console.log(`agora são ${horas} da tarde`)
-} else 
+} else if(horas >= 19 && horas <= 23){
+    console.log(`agora são ${horas} da noite`)
+} else {
+    console.log('digite um horario valido...')
+}
