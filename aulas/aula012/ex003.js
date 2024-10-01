34709cc48458bf9904a8fc5ac5e5ce13f6823591
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-10-01 16:21:11 -0300

    criando exercicico

---
+++ aulas/aula012/ex003.js
@@ -0,0 +1,8 @@
+var horas 
+if (horas <= 6) {
+    console.log(`agora são ${horas} da madrugada`)
+} else if (horas <= 11) {
+    console.log(`agora são ${horas} da manha`)
+} else if (horas <= 18) {
+    console.log(`agora são ${horas} da tarde`)
+} else 
