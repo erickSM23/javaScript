3aa02b9038b488938bd876df8b209c49d5878736
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-10-01 16:08:47 -0300

    exercício de idade finalizado com êxito

---
+++ aulas/aula012/ex002.js
@@ -0,0 +1,14 @@
+var idade = 343534524
+if (idade < 16) {
+    console.log('você não pode votar com essa idade')
+} else if (idade < 18) {
+    console.log('seu voto é opicional')
+} else if (idade >= 18 && idade <= 65) {
+    console.log('seu voto é obrigatorio')
+} else if (idade > 65 && idade <= 100) {
+    console.log('voto opicional')
+} else if(idade > 100){
+    console.log('você atingiu o limite maximo. só vivos votam...')
+} else {
+    console.log('porfavor digite sua idade em numeros. caracteres não são aceitos na area de idade')
+}
