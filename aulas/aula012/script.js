9226e93a56fb52a17046fcf11a06a6d8c9a62e2e
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-10-04 17:58:43 -0300

    exercício de programação
    adicionei o javaScript externo por meio do codigo html e adicionei estilo css externo ao codigo html para criar uma interface responsiva ao horario atual do computador.

--- aulas/aula012/script.js
+++ aulas/aula012/script.js
@@ -1,1 +1,22 @@
-window.alert('vai tomando segue plana desde a criação')
+function carregar() {
+    var mensagem = window.document.getElementById('mensagem')
+    var imagem = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    mensagem.innerText = `agora são ${hora} horas`
+    mensagem.style.textAlign = 'center'
+    if (hora >= 0 && hora <= 11) {
+        //bom dia
+        imagem.style.backgroundImage = 'url("imagens/manha.jpg")'
+        window.document.body.style.backgroundColor = '#FACF1A'
+    }else if (hora >= 12 && hora <= 18) {
+        //boa tarde
+        imagem.style.backgroundImage = 'url("imagens/tarde.jpg")'
+        window.document.body.style.backgroundColor = '#7F94B1'
+    }else {
+        //boa noite
+        imagem.style.backgroundImage = 'url("imagens/noite.jpg")'
+        window.document.body.style.backgroundColor = '#071C2D'
+    }
+
+}
