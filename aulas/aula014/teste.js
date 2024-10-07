04ae5486b04f3ea4a89c3d1aa749f52796bdd5ba
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-10-07 19:11:33 -0300

    exercicio completo

---
+++ aulas/aula014/teste.js
@@ -0,0 +1,5 @@
+console.log('CONTAGEM REGRESIVA EM')
+for (var contador = 10;contador > 0;contador--) {
+    console.log(contador)
+}
+console.log('PARTIU! PARTIU! LIGO! LIGOU! FOI! FOI QUE FOI! VAIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII JAME WEBBBBBBB! FOI QUE FOI PARTIU! (CORTARAM PARA O CGI?)')
