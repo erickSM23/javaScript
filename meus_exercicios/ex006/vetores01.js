4d5b1e120d2de7559b4dbbf3d9ef48e8f9f8bc6f
erickSM23 <ericksouzadearaujo19@gmail.com>
2024-10-21 15:53:12 -0300

    exercício finalizado com êxito

---
+++ meus_exercicios/ex006/vetores01.js
@@ -0,0 +1,5 @@
+var vetor1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 110, 130, 140]
+vetor1.sort()
+for (var contador = 0; contador <= 13; contador ++) {
+    console.log(vetor1[contador])
+}
